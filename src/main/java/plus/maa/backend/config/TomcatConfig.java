42517689989ecabb34db5c021ae26d70f5a4772a
lixuhuilll <676824363@qq.com>
2024-01-04 19:57:15 +0800

    同时适配 Servlet 和 Reactive

--- src/main/java/plus/maa/backend/config/TomcatConfig.java
+++ src/main/java/plus/maa/backend/config/TomcatConfig.java
@@ -1,5 +1,7 @@
 package plus.maa.backend.config;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatReactiveWebServerFactory;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.context.annotation.Bean;
@@ -9,6 +11,13 @@ import org.springframework.context.annotation.Configuration;
 public class TomcatConfig {
 
     @Bean
+    @ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.REACTIVE)
+    public WebServerFactoryCustomizer<TomcatReactiveWebServerFactory> maaTomcatReactiveCustomizer() {
+        return factory -> factory.setProtocol("org.apache.coyote.http11.Http11Nio2Protocol");
+    }
+
+    @Bean
+    @ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.SERVLET)
     public WebServerFactoryCustomizer<TomcatServletWebServerFactory> maaTomcatServletCustomizer() {
         return factory -> factory.setProtocol("org.apache.coyote.http11.Http11Nio2Protocol");
     }
